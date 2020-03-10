07e71e8db236aa4342aa1ebcdbbba36e0d019926
Christian-Windows <christian.winwood@aggiemail.usu.edu>
2020-03-09 20:10:57 -0600

    Updated 4700 repo

---
+++ HW5_HW6/Assn5.py
@@ -0,0 +1,5 @@
+#BNF
+#<expression> ::= <booleanExpression> || <numberExpression> || <number> || <mathExpression>
+#<mathExpression> ::= <operator> <mathExpression> <mathExpression> || <number>
+#<number> ::= integer
+#<booleanExpression> ::= <booleanOperator> <booleanExpression> <booleanExpression> || <booleanNotOperator> <booleanExpression> || <number>
